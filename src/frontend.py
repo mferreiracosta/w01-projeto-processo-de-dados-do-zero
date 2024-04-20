60a326a8850b0cdb97571125ca8a5ad74aad16b4
Matheus Costa <matheeusferreeira@hotmail.com>
2024-04-20 11:16:19 -0300

    fix: ajustar título do dashboard. Testando PR

--- src/frontend.py
+++ src/frontend.py
@@ -12,7 +12,7 @@ class ExcelValidatorUI:
         )
 
     def display_header(self):
-        st.title("Modificação do título")
+        st.title("Insira o excel para validação")
 
     def upload_file(self):
         return st.file_uploader("Carregue seu arquivo excel aqui", type=["xlsx"])
